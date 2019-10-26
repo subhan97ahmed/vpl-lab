a641c2310435ce555e0d381a5560989d78b3c40f
subhan97ahmed <55913318+subhan97ahmed@users.noreply.github.com>
2019-10-26 23:30:35 +0500

    Add files via upload

---
+++ lab 2/Q1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            int[] ar = { 1, 23, 5, 3, 3, 5 };
+            for (int i = 0; i < ar.Length; i++)
+            {
+                count = 0;
+                for (int r = 0; r < ar.Length; r++)
+                {
+                    if (ar[i] == ar[r])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(ar[i]+" is "+count+" times");
+            }
+        }
+    }
+}
