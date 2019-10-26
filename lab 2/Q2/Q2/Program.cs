2846a0125f761e691d806f2f4c49eb51a0a39dc6
subhan97ahmed <55913318+subhan97ahmed@users.noreply.github.com>
2019-10-26 23:32:04 +0500

    Add files via upload

---
+++ lab 2/Q2/Q2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    //2.	Write a program to find maximum and minimum element in an array.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Q2";
+            int size = 5;
+            Console.WriteLine("Enter the size of an array");
+            size = int.Parse(Console.ReadLine());
+            int[] num = new int[size];
+            Console.WriteLine("Enter the elements of array");
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            int max = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]>=max)
+                {
+                    max = num[i];
+                }
+               
+            }
+            int min = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]<min)
+                {
+                    min = num[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("the max value " + max + "\nthe min values " + min);
+            // or u can do by bulit in methods
+            Console.WriteLine("\n");
+            Console.WriteLine("the max value is "+num.Max());
+            Console.WriteLine("the min value is "+num.Min());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab 2/Q3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    //Q3 write a program to spearate out the odd and even numbers in and array in two different arrays
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[5];
+
+            Console.WriteLine("enter numbers");
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = int.Parse(Console.ReadLine());
+            }
+            int evencount = 0;
+            int oddcount = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0)
+                {
+                    evencount++;
+                }
+                else
+                {
+                    oddcount++;
+                }
+
+            }
+
+            int[] even = new int[evencount+1];
+            int[] odd = new int[oddcount+1];
+            int r = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+
+                if (ar[i] % 2 == 0)
+                {
+                    even[r] = ar[i];
+                    r++;
+                }
+                
+
+            }
+            r = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if(ar[i]%2!=0)
+                {
+                    odd[r] = ar[i];
+                    r++;
+                }
+            }
+            
+
+            Console.WriteLine("even\todd");
+            for (int i = 0; i < evencount; i++)
+            {
+                Console.WriteLine(even[i]); ;
+            }
+            for (int i = 0; i < oddcount; i++)
+            {
+                Console.WriteLine("\t"+odd[i]);
+            }
+        }
+    }
+}
