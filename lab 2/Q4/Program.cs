ac2cb6549aaccd28b5cb2cf395119814a28ed10b
subhan97ahmed <55913318+subhan97ahmed@users.noreply.github.com>
2019-10-26 23:33:38 +0500

    Add files via upload

---
+++ lab 2/Q4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    //Q4 write a program to find the length of the String without using length libaray 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Enter any String to find its length");
+            a= Console.ReadLine();
+            Console.WriteLine("the length is "+count(a));
+        }
+
+        public static int count(String a) {
+            int count = 0;
+            foreach (char c in a)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ lab 2/Q5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    //Q5 write a program  to count the total number of words in String 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int words=0;
+            String name = "subhan ahmed.";
+            Console.WriteLine("The string is "+name);
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i]==' ' || name[i]=='.')
+                {
+                    words++;
+                }
+            }
+            Console.WriteLine("The number of words are "+words);
+        }
+    }
+}
